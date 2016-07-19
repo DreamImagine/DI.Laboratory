aff3eff5b3c1caf347eafbccd43bd03de61bd6b1
HuJiaming <hujiaming@szpayu.com>
2016-07-19 13:29:58 +0800

    新增 Web站点

---
+++ DI.L.Changeable.WebSite/Models/ManageViewModels/FactorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DI.L.Changeable.WebSite.Models.ManageViewModels
+{
+    public class FactorViewModel
+    {
+        public string Purpose { get; set; }
+    }
+}
